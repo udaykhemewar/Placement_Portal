85fc8da0e1a9cd63860c063cde583f2ea4841047
Doallpatari <yash.patari007@gmail.com>
2023-12-20 00:35:17 +0530

    Flyway Deleted

--- db/migration/V2__Add_Column.sql
+++
@@ -1,8 +0,0 @@
-create table public.attendance (
-                                   id bigint primary key not null default nextval('attendance_id_seq'::regclass),
-                                   attendance boolean,
-                                   date timestamp(6) without time zone,
-                                   employee_id bigint,
-                                   foreign key (employee_id) references public.employees2 (id)
-                                       match simple on update no action on delete no action
-);
